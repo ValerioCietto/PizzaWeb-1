6ef91bd15efdd3188e657bfde37201772bc8cb85
mirko <mirko@mirko-KP266AA-ABZ-a6431-it>
2014-05-29 23:28:13 +0200

    modificato model

--- PizzaWeb/src/java/mypackage/Model.java
+++ PizzaWeb/src/java/mypackage/Model.java
@@ -5,13 +5,24 @@ import javax.servlet.http.*;
 import java.lang.*;
 
 public class Model {
+    /**
+     * Estrapola dalla request gli input password e utente
+     * Controlla che non siano nulli
+     * Crea un'oggetto utente(con parametri vuoti se usr o pwd errati)
+     * Controlla la riuscita del punto precedente
+     * Carica i parametri nella sessione
+     * //////////
+     * -da aggiungere il controllo del se è gia connesso
+     * -forse passare oggetto invece che stringhe
+     * @param req 
+     */
     public static void login(HttpServletRequest req){
         HttpSession s=req.getSession();
         String name = req.getParameter("login");
         String password = req.getParameter("password");
         if(name!=null && password!=null && !name.equals("") && !password.equals("")){
             Utente login = new Utente( DBManager.getLogin(name,password) );
-            if(!login.getNome().equals("")){
+            if(login.getNome()!=null && !login.getNome().equals("")){
                 s.setAttribute("username",login.getNome());
                 s.setAttribute("ruolo", login.getRuolo());
                 s.setAttribute("message","login effettuato, benvenuto!");
@@ -20,21 +31,40 @@ public class Model {
         }else
             s.setAttribute("message","inserisci il tuo nome utente e la tua password.");
     }
+    /**
+     * Invalida i dati di sessione
+     * Genera una nuova sessione
+     * @param req 
+     */
     public static void logout(HttpServletRequest req){
         HttpSession s=req.getSession();
         s.invalidate();
         s=req.getSession();
         s.setAttribute("message", "logout effettuato");
     }
+    /**
+     * Estrapola il nome della pizza da eliminare
+     * Richiama il DB per rimuovare la pizza
+     * ---------------------
+     * controllare che pizza non sia nullo
+     * @param req 
+     */
     public static void remPizza(HttpServletRequest req){
         String pizza= req.getParameter("pizza");
         if(!DBManager.remPizza(pizza))
             (req.getSession()).setAttribute("message","Problema sql");
     }
+    /**
+     * Estrapola il nome, ingredienti e prezzo della pizza da aggiungere
+     * Crea un'oggetto pizza con i parametri sopra(nel caso non riesca i valori sono vuoti)
+     * Controlla la riuscita della creazione della pizza
+     * Carica la pizza nel DB
+     * @param req 
+     */
     public static void addPizza(HttpServletRequest req){
         HttpSession s=req.getSession();
         Pizza temp=new Pizza(req.getParameter("pizza"),req.getParameter("ingredienti"),req.getParameter("prezzo"));
-        if (temp.getNome()!=null){
+        if (temp.getNome()!=null && !temp.getNome().equals("")){
             String n= temp.getNome();
             String i= req.getParameter("ingredienti");
             Double p= temp.getPrezzo();
@@ -56,17 +86,26 @@ public class Model {
         String ruolo= "user";
         DBManager.addLogin(n,p, ruolo); //To change body of generated methods, choose Tools | Templates.
     }
+    /**
+     * Estrapola il nome della pizza e la quantità, il nome del cliente e l'ora di prenotazione
+     * Carica un solo tipo di pizza nel database
+     * 
+     * ///////////////
+     * Manca creazione oggetto Prenotazione
+     * Manca controllo dei dati
+     * 
+     * NOTA ANNA: ALLORA, QUANDO SI CLICCA IL PRENOTA VIENE INVIATO SOLO LA PIZZA CORRISPONDENTE QUINDI NON BISOGNA USARE ARRAY PORCA TROTA!!!!!!
+     * @param req 
+     */
     static void addPren(HttpServletRequest req) {
-        
-        //ALLORA, QUANDO SI CLICCA IL PRENOTA VIENE INVIATO SOLO LA PIZZA CORRISPONDENTE QUINDI NON BISOGNA USARE ARRAY PORCA TROTA!!!!!!
-       
         String p=req.getParameter("pizza");
         String clName=(String)req.getSession().getAttribute("username");
         int q= (Integer.parseInt(req.getParameter("quant"))); //questo gli riempie solo un numero
         String d= req.getParameter("data");
+        
         DBManager.addPrenotazioneanna(clName,d, p, q);
-   
     }
+    
     static void remPren(HttpServletRequest req){
         
         //in teoria name non dovrebbe mai essere nullo se lo facciamo vedere dalla pagina prenot, accessibile solo dall'utente registrato
@@ -85,43 +124,76 @@ public class Model {
     
 }
 class Pizza{
-    String nome;
-    String ingredienti;
-    double prezzo;
+    String nome="";
+    String ingredienti="";
+    double prezzo=0;
+    /**
+     * Controlla che gli input non siano nulli o non accettabili
+     * In caso di non riuscita i parametri sono nulli
+     * Altrimenti carica i dati nell'oggetto
+     * 
+     * @param iNome
+     * @param iIngredienti
+     * @param iPrezzo 
+     */
     public Pizza(String iNome,String iIngredienti,double iPrezzo){
         if(iNome!=null && !iNome.equals("")){
-            if(iIngredienti!=null){
+            if(iIngredienti!=null && !iIngredienti.equals("")){
                 if (iPrezzo>0){
                     ingredienti=iIngredienti;
                     nome=iNome;
                     prezzo=iPrezzo;
                 }}}
     }
+    /**
+     * Parsifica la stringa del testo
+     * Controlla che gli input non siano nulli o non accettabili
+     * In caso di non riuscita i parametri sono nulli
+     * Altrimenti carica i dati nell'oggetto
+     * 
+     * @param iNome
+     * @param iIngredienti
+     * @param iPrezzo 
+     */
     public Pizza(String iNome,String iIngredienti,String iPrezzo){
-    try{
+        try{
             double tempPrezzo= Double.parseDouble(iPrezzo);
             if(iNome!=null && !iNome.equals("")){
-            if(iIngredienti!=null){
-                if (tempPrezzo>0){
-                    ingredienti=iIngredienti;
-                    nome=iNome;
-                    prezzo=tempPrezzo;
-                }}}
+                if(iIngredienti!=null && !iIngredienti.equals("")){
+                    if (tempPrezzo>0){
+                        ingredienti=iIngredienti;
+                        nome=iNome;
+                        prezzo=tempPrezzo;
+                    }}}
                   
-       }catch(NumberFormatException e){
-                    System.out.println(e);
-                }
+        }catch(NumberFormatException e){
+            System.out.println(e);
+        }
     }
+    /**Restituisce il prezzo della pizza
+     * @return <double> prezzo
+     */
     public double getPrezzo(){
         return prezzo;
     }
+    /**Restituisce il nome della pizza
+     * @return <String> nome pizza
+     */
     public String getNome(){
     return nome;
     }
+    /**
+     * Controlla che la pizza su cui stai lavorando esista
+     * Controlla che i dati di input siano accettabili
+     * Aggiorna i dati della pizza nel DB
+     * @param nIngredienti
+     * @param nPrezzo 
+     */
     public void setPizza(String nIngredienti,double nPrezzo){
         String oNome=this.nome;
-        DBManager.modPizza(oNome, nIngredienti, nPrezzo);
-
+        if(oNome!=null && !oNome.equals(""))
+            if(nIngredienti!=null && !nIngredienti.equals("") && nPrezzo>0)
+                DBManager.modPizza(oNome, nIngredienti, nPrezzo);
     }
    
 }
@@ -129,11 +201,21 @@ class Utente{
     String nome="";
     String pwd="";
     String ruolo="";
-    public Utente(String iNome,String iPwd, String iRuolo){
+    /**
+     * NON USATO
+     * @param iNome
+     * @param iPwd
+     * @param iRuolo 
+     */
+    /*public Utente(String iNome,String iPwd, String iRuolo){
         nome=iNome;
         pwd=iPwd;
         ruolo=iRuolo;
-    }
+    }*/
+    /**
+     * Crea l'utente
+     * @param input array con nome,pwd e ruolo 
+     */
     public Utente(String []input){
         if(input==null)
             return;
@@ -141,17 +223,49 @@ class Utente{
         pwd=input[1];
         ruolo=input[2];
     }
+    /**
+     * Restituisce il nome dell'utente
+     * @return <String> nome Pizza
+     */
     public String getNome(){
         return nome;
     }
+    /**
+     * Restituisce la password dell'utente
+     * @return <String>
+     */
     public String getPwd(){
         return pwd;
     }
+    /**
+     * Restituisce il ruolo dell'utente
+     * @return <String>
+     */
     public String getRuolo(){
         return ruolo;
     }
 }
-class Ordine extends Pizza{
+/**
+ * da finire e adeguare
+ * @author mirko
+ */
+class Prenotazione{
+    String cliente="";
+    String data="";
+    String pizza="";
+    int quantita=0;
+    String stato="";
+    public Prenotazione(String iCliente,String iData, String iPizza, int iQuantita){
+        if(iCliente!=null && !iCliente.equals("") && iData!=null && !iData.equals("") && iPizza!=null && !iPizza.equals("") && iQuantita>0){
+            cliente=iCliente;
+            data=iData;
+            pizza=iPizza;
+            quantita=iQuantita;
+            stato="Ordinata";
+        }
+    }
+}
+/*class Ordine extends Pizza{
     int quantita=1;   
     public Ordine(String iNome,String iIngredienti, double iPrezzo){
         super(iNome, iIngredienti, iPrezzo);
@@ -184,5 +298,4 @@ class Prenotazione{
     public Ordine getOrdine(int i){
         return prenotaz[i];
     }
-}
-
+}*/
